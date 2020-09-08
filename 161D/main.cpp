35db74e6ee68588add8e4d27d6f5c45296ef0646
TheRiseOfDavid <david53133@gmail.com>
2020-09-09 01:28:18 +0800

    20200909.0128

--- 161D/main.cpp
+++ 161D/main.cpp
@@ -1,113 +1,95 @@
-#include <cstdio>
-#include <algorithm>
+#include<iostream>
+#include<bits/bits/stdc++.h>
 #define LOCAL
-using namespace std;
+#define MAXN 50005
+int n , k , a , b ;
+int ans , cnt ;
+int Max[MAXN] , sz[MAXN] , rt ;
+int head[MAXN], dis[MAXN];
+bool vis[MAXN] ;
+struct node{
+    int v , nx ;
+}Edge[MAXN];
 
-const int M = (int)5e4;
-
-int n, k, ans;
-
-int cnt;
-int head[M + 5];
-struct node
-{
-    int v, nx;
-}Edge[M * 2 + 5];
-
-int dis[M + 5];
-bool vis[M + 5];
-int rt, sz[M + 5], Max[M + 5];
-
-void init(int n)
-{
-    Max[0] = n, ans = cnt = 0;
-    for(int i = 1; i <= n; ++i)
-    {
-        vis[i] = 0;
-        head[i] = -1;
-    }
-
-}
-
-void add(int u, int v)
-{
-    Edge[cnt].v = v;
-    Edge[cnt].nx = head[u];
-    head[u] = cnt++;
-    int sto = 1 ;
-    sto=3;
-}
-
-void get_rt(int u, int fa)
-{
-    sz[u] = 1, Max[u] = 0;
-    for(int i = head[u]; ~i; i = Edge[i].nx)
-    {
-        int v = Edge[i].v;
-        if(v == fa || vis[v])   continue;
-        get_rt(v, u);
-        sz[u] += sz[v];
-        Max[u] = max(Max[u], sz[v]);
-    }
-    Max[u] = max(Max[u], n - sz[u]);
-    if(Max[u] < Max[rt])
-        rt = u;
-}
-
-void get_dis(int u, int fa, int d)
-{
-    for(int i = head[u]; ~i; i = Edge[i].nx)
-    {
-        int v = Edge[i].v;
-        if(v == fa || vis[v])   continue;
-        dis[++cnt] = d + 1;
-        get_dis(v, u, dis[cnt]);
+void init(int n ){
+    Max[0] = n ;
+    ans = cnt = 0 ;
+    for(int i = 0 ; i <= n ; i++){
+        head[i] = -1 ;
+        vis[i].v = 0 ;
     }
 }
 
-int get_ans(int u, int d)
-{
-    dis[cnt = 1] = d, get_dis(u, 0, d), sort(dis + 1, dis + cnt + 1);
-    int l = 1, ans = 0;
-    while(l < cnt && dis[l] + dis[cnt] < k) ++l;
-    while(l < cnt && dis[l] <= k - dis[l])
-    {
-        ans += upper_bound(dis + l + 1, dis + cnt + 1, k - dis[l]) - lower_bound(dis + l + 1, dis + cnt + 1, k - dis[l]);
-        ++l;
-    }
-    return ans;
+void add(int u , int v){
+    Edge[cnt].v = v ;
+    Edge[cnt].nx = head[u] ;
+    head[u] = cnt++ ;
 }
 
-void dfs(int u)
-{
-    vis[u] = 1;
-    ans += get_ans(u, 0);
-    for(int i = head[u]; ~i; i = Edge[i].nx)
-    {
-        int v = Edge[i].v;
-        if(vis[v])  continue;
-        ans -= get_ans(v, 1);
-        n = sz[v], rt = 0, get_rt(v, u);
+void get_rt(int u , int fa ){
+    sz[u] = 1 ; Max[u] = 0 ;
+    for(int i = head[u] ; ~i ; Edge[i].nx){
+        int v = Edge[i].v ;
+        if(vis[v] || v == fa )  continue ;
+        get_rt(v,u);
+        sz[u] += sz[v] ;
+        Max[u] = max(Max[u] , sz[v]);
+    }
+    Max[u] = max(Max[u] , n - sz[u])
+    if(Max[rt] > Max[u])
+        rt =  u ;
+}
+
+void get_dis(int u , int fa , int d){
+    for(int i = head[u] ; ~i ; Edge[i].nx){
+        int v = Edge[i].v ;
+        if(vis[v] || v == fa ) continue ;
+        dis[++cnt] = d + 1 ;
+        get_dis(v,u,dis[cnt]);
+    }
+}
+
+
+int get_ans(int u , int d ){
+    dis[cnt=1] = d ;
+    get_dis(u,0,d) ;
+    sort(dic+1 , dic+cnt+1) ;
+    int l = 1 , ans = 0 ;
+
+    while(l < cnt && dis[l] + dis[cnt] < k ) l++ ;
+    while(l < cnt && dis[l] <= k - dis[l]){
+        ans += upper_bound(dis + l + 1 , dis + cnt + 1 , k - dis[l]) - lower_bound(dis+l+1 , dis+cnt+1 , k-dis[l]);
+        l++ ;
+    }
+    return ans ;
+}
+
+void dfs(int u ){
+    vis[u] = 1 ;
+    ans += get_ans(rt , 0);
+    for(int i = head[u] ; ~i ; i < Edge[i].nx){
+        int v = Edge[i].v ;
+        if(vis[v]) continue ;
+        ans -= get_ans(v , 1) ;
+        n = sz[v] , rt = 0  , get_rt(1,0);
         dfs(rt);
     }
 }
 
-int main()
-{
+int main(){
 #ifdef LOCAL
-freopen("in1.txt","r",stdin);
-#endif
+    freopen("in1.txt" , "r" , stdin);
+#endif // LOCAL
 
-    scanf("%d %d", &n, &k);
+    cin >> n >> k ;
     init(n);
-    for(int i = 0, u, v; i < n - 1; ++i)
-    {
-        scanf("%d %d", &u, &v);
-        add(u, v);
-        add(v, u);
+    for(int i ; i < n ; i++){
+        cin >> a >> b ;
+        add(a,b);
+        add(b,a);
     }
-    rt = 0, get_rt(1, 0);
+    rt = 0 ; get_rt(1,0);
     dfs(rt);
-    printf("%d\n", ans);
-    return 0;
+    cout << ans << '\n' ;
+
 }
