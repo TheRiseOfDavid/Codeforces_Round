2b09dbde0f8f714d27fc7065cc74b741195a1d93
TheRiseOfDavid <david53133@gmail.com>
2021-02-10 00:08:30 +0800

    20210210.0008

--- 1474D/main.cpp
+++ 1474D/main.cpp
@@ -1,85 +1,52 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 #define LOCAL
 #define MAXN 200020
-using namespace std ;
-int t,n ;
-int l[MAXN], pre[MAXN] ;
-int r[MAXN], suf[MAXN] ;
+#define int long long
+#define INF 0x3f3f3f
+using namespace std;
+int t, n, num[MAXN];
+int pre[MAXN], suf[MAXN];
 
-int dl(){
-    int flag = 1 ;
-    for(int i = 1 ; i <= n ; i++ ){
-        pre[i] = l[i] - pre[i-1] ;
-        if(pre[i] < 0 ){
-            if(flag==0)
-                return 0 ;
-            flag = 0 ;
-            pre[i-1] = l[i] - pre[i-2];
-            pre[i] = l[i-1] - pre[i-1];
-        }
+int solve(){
+
+    for(int i = 1; i < n; i++){
+        if(suf[i+1] == pre[i])
+            return 1;
+        int x = num[i+1] - pre[i-1], y = num[i] - suf[i+2] ;
+        if(x >= 0 && y >= 0 && x == y)
+            return 1;
     }
-    /*
-    cout << "pre ";
-    for(int i = 1; i <= n; i++)
-        cout << pre[i] << ' ' ;
-    cout << '\n';
-     */
-    if(pre[n] == 0) return 1 ;
-    return 0 ;
+    return 0;
 }
 
-int dr(){
-    int flag = 1 ;
-    suf[n+1] = 0;
-    for(int i = n ; i > 0 ; i-- ){
-        suf[i] = r[i] - suf[i+1] ;
-        //cout << suf[i] << ' ' ;
-        if(suf[i] < 0 ){
-            if(flag==0)
-                return 0 ;
-            flag = 0 ;
-            suf[i+1] = r[i] - suf[i+2];
-            suf[i] = r[i+1] - suf[i+1];
-        }
-    }
-    /*
-    cout << "suf ";
-    for(int i = 1; i <= n; i++)
-        cout << suf[i] << ' ' ;
-    cout << '\n';
-    */
-    if(suf[1] == 0) return 1 ;
-    return 0 ;
-}
-
-int debug(){
-    cout << "n is " << n << '\n' ;
-    for(int i = 1 ; i <= n ; i++){
-        cout << l[i] << ' ' ;
-    }
-    cout << "\n" ;
-    for(int i = n ; i > 0 ; i--)
-        cout << r[i] << ' ' ;
-    cout << "\n\n" ;
-}
-
-
-int main(){
+int32_t main(){
 #ifdef LOCAL
-    freopen("in1.txt" , "r" , stdin );
+    freopen("in1.txt", "r", stdin);
 #endif // LOCAL
-    cin >> t ;
+
+    cin >> t;
     while(t--){
-        cin >> n ;
-        int temp = n-1 ;
-        for(int i = 1 ; i <= n ; i++){
-            cin >> l[i] ;
-            r[i] = l[i] ;
+        cin >> n;
+        for(int i = 1; i <= n; i++)
+            cin >> num[i];
+        suf[n+1] = 0;
+        pre[0] = 0;
+        for(int i = 1; i <= n; i++){
+            if(num[i] - pre[i-1] < 0)
+                pre[i] = INF;
+            else
+                pre[i] = num[i] - pre[i-1];
         }
-        if(dl() == 1 || dr() == 1 )
-            cout << "YES\n" ;
+        for(int i = n; i > 0; i--){
+            if(num[i] - suf[i+1] < 0)
+                suf[i] = INF + MAXN;
+            else
+                suf[i] = num[i] - suf[i+1];
+        }
+        if(solve())
+            cout << "YES\n";
         else
-            cout << "NO\n" ;
-        //debug() ;
+            cout << "NO\n";
     }
+
 }
